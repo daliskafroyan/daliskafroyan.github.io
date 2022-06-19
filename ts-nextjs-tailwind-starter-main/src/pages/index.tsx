4bda70056056fc9816dcc6f762ef08221b9596a6
firstaroyand <firstaroyan09@gmail.com>
2022-06-19 23:29:59 +0700

    feat(homepage): add post section

--- ts-nextjs-tailwind-starter-main/src/pages/index.tsx
+++ ts-nextjs-tailwind-starter-main/src/pages/index.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
 import NextImage from '@/components/NextImage';
 import Seo from '@/components/Seo';
 
-import Ava from '~/images/first-avatar.png'
+import Ava from '~/images/first-avatar.png';
 /**
  * SVGR Support
  * Caveat: No React Props Type.
@@ -24,40 +20,62 @@ export default function HomePage() {
       <Seo />
 
       <main className='space-y-16'>
-          <div className="layout">  
-            <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-x-16">
-              <div className="flex flex-col justify-center">
-              <NextImage src={Ava} useSkeleton alt='avatar image' width='180' height='245' layout='responsive'/> 
+        <div className='layout'>
+          <div className='flex flex-col justify-center space-y-4 md:flex-row md:space-x-16'>
+            <div className='flex flex-col justify-center'>
+              <NextImage
+                src={Ava}
+                useSkeleton
+                alt='avatar image'
+                width='180'
+                height='245'
+                layout='responsive'
+              />
+            </div>
+            <div className='flex flex-col md:space-y-4'>
+              <div className='flex flex-col justify-center'>
+                <h1>Hi! I&apos;m Royan</h1>
+                <h1>A Frontend Engineer</h1>
+                <h1>That cares about</h1>
+                <h1>Productivity and</h1>
+                <h1>Efficiency</h1>
               </div>
-              <div className="flex flex-col md:space-y-4">
-                <div className="flex flex-col justify-center">
-                  <h1>Hi! I&apos;m Royan</h1>
-                  <h1>A Frontend Engineer</h1>
-                  <h1>That cares about</h1>
-                  <h1>Productivity and</h1>
-                  <h1>Efficiency</h1>
-                </div>
-                <div className="flex flex-col justify-center">
-                  <p>I&apos;m specialized in building app that favors </p>
-                  <p>both user and developer by leveraging</p>
-                  <p>right tools to get things done.</p>
-                </div>
-
+              <div className='flex flex-col justify-center'>
+                <p>I&apos;m specialized in building app that favors </p>
+                <p>both user and developer by leveraging</p>
+                <p>right tools to get things done.</p>
               </div>
             </div>
           </div>
-          <div className='layout'>
-            <div className='m-auto'>
-            <div className='bg-indigo-500 z-0 absolute w-4/5 h-1/3 justify-center' />
-
-            </div>
-            <div className='z-10 relative'>
+        </div>
+        <section className='layout'>
+          <div className='relative'>
+            <div className='absolute top-5 left-5 z-0 h-52 w-10/12 justify-center bg-indigo-200' />
+          </div>
+          <div className='relative z-10'>
             <h2>Featured Post</h2>
             <p>Some of my documentations that worth to read</p>
-
-            </div>
-
           </div>
+          <div className='flex flex-col'>
+            <div className='relative h-72 w-72 bg-white bg-opacity-30 p-3 backdrop-blur-sm backdrop-filter'>
+              <div className='absolute p-3'>
+                <h3 className='leading-6'>
+                  leveraging typescript generic feature
+                </h3>
+                <div className='flex justify-between'>
+                  <p>11 min read</p>
+                  <p>September 21, 2021</p>
+                </div>
+                <div>
+                  <p>
+                    Generating this type of typescript could easily said, the
+                    best thing you learn in TS
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </Layout>
   );
