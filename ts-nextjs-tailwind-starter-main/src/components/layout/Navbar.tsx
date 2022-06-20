f9e586db6f81c8e278fb685a2252685fe6629339
firstaroyand <firstaroyan09@gmail.com>
2022-06-20 23:13:10 +0700

    feat(homepage): finishing basic homepage

--- ts-nextjs-tailwind-starter-main/src/components/layout/Navbar.tsx
+++ ts-nextjs-tailwind-starter-main/src/components/layout/Navbar.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-import UnstyledLink from '@/components/links/UnstyledLink';
-
 const links = [
   { href: '/', label: 'Route 1' },
   { href: '/', label: 'Route 2' },
@@ -9,18 +7,18 @@ const links = [
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30">
-    <div className="max-w-5xl mx-auto px-4">
-      <div className="flex items-center justify-between h-16">
-        <span className="text-2xl text-gray-900 font-semibold">Logo</span>
-        <div className="flex space-x-4 text-gray-900">
-          <a href="#">Dashboard</a>
-          <a href="#">About</a>
-          <a href="#">Projects</a>
-          <a href="#">Contact</a>
+    <nav className='sticky top-0 z-20 bg-white bg-opacity-30 backdrop-blur-lg backdrop-filter'>
+      <div className='mx-auto max-w-5xl px-4'>
+        <div className='flex h-16 items-center justify-between'>
+          <span className='text-2xl font-semibold text-gray-900'>Logo</span>
+          <div className='flex space-x-4 text-gray-900'>
+            <a href='#'>Dashboard</a>
+            <a href='#'>About</a>
+            <a href='#'>Projects</a>
+            <a href='#'>Contact</a>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
 }
--- ts-nextjs-tailwind-starter-main/src/pages/index.tsx
+++ ts-nextjs-tailwind-starter-main/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ImGithub, ImLinkedin, ImTwitter } from 'react-icons/im';
 
 import Layout from '@/components/layout/Layout';
 import NextImage from '@/components/NextImage';
@@ -19,10 +20,10 @@ export default function HomePage() {
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
 
-      <main className='space-y-16'>
+      <main className='space-y-16 md:space-y-44'>
         <div className='layout'>
-          <div className='flex flex-col justify-center space-y-4 md:flex-row md:space-x-16'>
-            <div className='flex flex-col justify-center'>
+          <div className='!mt-4 flex flex-col items-center justify-center space-y-4 sm:mt-0 sm:flex-row sm:space-x-16 md:!mt-44'>
+            <div className='align-center flex flex-col justify-center'>
               <NextImage
                 src={Ava}
                 useSkeleton
@@ -34,11 +35,20 @@ export default function HomePage() {
             </div>
             <div className='flex flex-col md:space-y-4'>
               <div className='flex flex-col justify-center'>
-                <h1>Hi! I&apos;m Royan</h1>
-                <h1>A Frontend Engineer</h1>
-                <h1>That cares about</h1>
-                <h1>Productivity and</h1>
-                <h1>Efficiency</h1>
+                <h1 className='md:text-[2.45rem]'>Hi! I&apos;m Royan</h1>
+                <h1 className='md:text-[2.45rem]'>A Frontend Engineer</h1>
+                <h1 className='md:text-[2.45rem]'>That cares about</h1>
+                <h1 className='md:text-[2.45rem]'>
+                  <span className='signature-gradient animate-text bg-clip-text text-transparent'>
+                    Productivity{' '}
+                  </span>
+                  and
+                </h1>
+                <h1 className='md:text-[2.45rem]'>
+                  <span className='signature-gradient animate-text bg-clip-text text-transparent'>
+                    Efficiency
+                  </span>
+                </h1>
               </div>
               <div className='flex flex-col justify-center'>
                 <p>I&apos;m specialized in building app that favors </p>
@@ -50,33 +60,184 @@ export default function HomePage() {
         </div>
         <section className='layout'>
           <div className='relative'>
-            <div className='absolute top-5 left-5 z-0 h-52 w-10/12 justify-center bg-indigo-200' />
+            <div className='absolute top-5 left-5 z-0 h-[60rem] w-10/12 justify-center bg-indigo-100 md:h-52' />
           </div>
           <div className='relative z-10'>
             <h2>Featured Post</h2>
             <p>Some of my documentations that worth to read</p>
           </div>
-          <div className='flex flex-col'>
-            <div className='relative h-72 w-72 bg-white bg-opacity-30 p-3 backdrop-blur-sm backdrop-filter'>
+          <div className='flex flex-col items-center justify-center gap-6 pt-6 md:flex-row'>
+            <div className='relative h-[19rem] w-72 bg-white bg-opacity-30 p-3 backdrop-blur-sm backdrop-filter'>
               <div className='absolute p-3'>
-                <h3 className='leading-6'>
-                  leveraging typescript generic feature
-                </h3>
-                <div className='flex justify-between'>
-                  <p>11 min read</p>
-                  <p>September 21, 2021</p>
-                </div>
-                <div>
+                <div className='flex h-64 flex-col justify-between pr-3'>
+                  <div>
+                    <h3 className='signature-gradient bg-clip-text pb-4 text-[2rem] leading-7 text-transparent'>
+                      lorem ipsum dolor
+                    </h3>
+                    <div className='flex justify-between'>
+                      <p className='text-sm font-bold text-gray-500'>
+                        11 min read
+                      </p>
+                      <p className='text-sm font-bold text-gray-500'>
+                        Sep 21, 2021
+                      </p>
+                    </div>
+                  </div>
                   <p>
-                    Generating this type of typescript could easily said, the
-                    best thing you learn in TS
+                    lorem ipsum dolor could easily said, the best thing you
+                    learn in TS
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='relative h-[19rem] w-72 bg-white bg-opacity-30 p-3 backdrop-blur-sm backdrop-filter'>
+              <div className='absolute p-3'>
+                <div className='flex h-64 flex-col justify-between pr-3'>
+                  <div>
+                    <h3 className='signature-gradient bg-clip-text pb-4 text-[2rem] leading-7 text-transparent'>
+                      lorem ipsum dolor
+                    </h3>
+                    <div className='flex justify-between'>
+                      <p className='text-sm font-bold text-gray-500'>
+                        11 min read
+                      </p>
+                      <p className='text-sm font-bold text-gray-500'>
+                        Sep 21, 2021
+                      </p>
+                    </div>
+                  </div>
+                  <p>
+                    lorem ipsum dolor could easily said, the best thing you
+                    learn in TS
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='relative h-[19rem] w-72 bg-white bg-opacity-30 p-3 backdrop-blur-sm backdrop-filter'>
+              <div className='absolute p-3'>
+                <div className='flex h-64 flex-col justify-between pr-3'>
+                  <div>
+                    <h3 className='signature-gradient bg-clip-text pb-4 text-[2rem] leading-7 text-transparent'>
+                      lorem ipsum dolor
+                    </h3>
+                    <div className='flex justify-between'>
+                      <p className='text-sm font-bold text-gray-500'>
+                        11 min read
+                      </p>
+                      <p className='text-sm font-bold text-gray-500'>
+                        Sep 21, 2021
+                      </p>
+                    </div>
+                  </div>
+                  <p>
+                    lorem ipsum dolor could easily said, the best thing you
+                    learn in TS
                   </p>
                 </div>
               </div>
             </div>
           </div>
+          <div className='flex justify-end pt-6'>
+            <button>
+              <p>See more &gt;</p>
+            </button>
+          </div>
+        </section>
+        <section>
+          <div className='relative'>
+            <div className='absolute z-0 h-52 w-full -skew-y-12 justify-center bg-indigo-100' />
+          </div>
+          <div className='layout'>
+            <div className='relative z-10 text-right'>
+              <h2>Featured Works</h2>
+              <p>Some of my notable projects</p>
+            </div>
+            <div className='flex flex-col items-center justify-center gap-6 pt-6 md:flex-row'>
+              <div className='relative h-[19rem] w-72 bg-white bg-opacity-30 p-3 backdrop-blur-sm backdrop-filter'>
+                <div className='absolute p-3'>
+                  <div className='flex h-64 flex-col justify-between pr-3'>
+                    <div>
+                      <h3 className='signature-gradient bg-clip-text pb-4 text-[2rem] leading-7 text-transparent'>
+                        lorem ipsum dolor
+                      </h3>
+                      <div className='flex justify-between'>
+                        <p className='text-sm font-bold text-gray-500'>
+                          11 min read
+                        </p>
+                        <p className='text-sm font-bold text-gray-500'>
+                          Sep 21, 2021
+                        </p>
+                      </div>
+                    </div>
+                    <p>
+                      lorem ipsum dolor could easily said, the best thing you
+                      learn in TS
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='relative h-[19rem] w-72 bg-white bg-opacity-30 p-3 backdrop-blur-sm backdrop-filter'>
+                <div className='absolute p-3'>
+                  <div className='flex h-64 flex-col justify-between pr-3'>
+                    <div>
+                      <h3 className='signature-gradient bg-clip-text pb-4 text-[2rem] leading-7 text-transparent'>
+                        lorem ipsum dolor
+                      </h3>
+                      <div className='flex justify-between'>
+                        <p className='text-sm font-bold text-gray-500'>
+                          11 min read
+                        </p>
+                        <p className='text-sm font-bold text-gray-500'>
+                          Sep 21, 2021
+                        </p>
+                      </div>
+                    </div>
+                    <p>
+                      lorem ipsum dolor could easily said, the best thing you
+                      learn in TS
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='relative h-[19rem] w-72 bg-white bg-opacity-30 p-3 backdrop-blur-sm backdrop-filter'>
+                <div className='absolute p-3'>
+                  <div className='flex h-64 flex-col justify-between pr-3'>
+                    <div>
+                      <h3 className='signature-gradient bg-clip-text pb-4 text-[2rem] leading-7 text-transparent'>
+                        lorem ipsum dolor
+                      </h3>
+                      <div className='flex justify-between'>
+                        <p className='text-sm font-bold text-gray-500'>
+                          11 min read
+                        </p>
+                        <p className='text-sm font-bold text-gray-500'>
+                          Sep 21, 2021
+                        </p>
+                      </div>
+                    </div>
+                    <p>
+                      lorem ipsum dolor could easily said, the best thing you
+                      learn in TS
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='flex justify-start pt-6'>
+              <button>
+                <p>See more &gt;</p>
+              </button>
+            </div>
+          </div>
         </section>
       </main>
+      <footer>
+        <div className='flex flex-row items-center justify-center gap-9 py-9'>
+          <ImTwitter fontSize={24} />
+          <ImLinkedin fontSize={24} />
+          <ImGithub fontSize={24} />
+        </div>
+      </footer>
     </Layout>
   );
 }
